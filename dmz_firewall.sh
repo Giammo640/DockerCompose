056aaf4a2c864e7a1e435952e631ec02148edb17
Giammo640 <g.lasalvia@studenti.unina.it>
2025-03-03 16:31:17 +0100

    Fixed firewall configuration script

--- dmz_firewall.sh
+++ dmz_firewall.sh
@@ -3,15 +3,24 @@
 #Install UFW if it is not already installed
 sudo apt install -y ufw
 
+echo "Fetching Facebook IP ranges..."
+IP_RANGES=$(whois AS32934 | grep -E "inetnum|NetRange" | awk '{print $2}')
 
-# Blocca tutto il traffico in uscita
-sudo ufw default deny outgoing
 
 # Blocca tutto il traffico in entrata
 sudo ufw default deny incoming
 
-# Block Facebook domain
-sudo ufw deny out to any app Facebook
+
+# Block each IP range
+echo "Blocking Facebook IPs with UFW..."
+for IP in $IP_RANGES; do
+    sudo ufw deny out to $IP
+    sudo ufw deny in from $IP
+    echo "Blocked: $IP"
+done
+
+sudo ufw allow in to any port 80
+sudo ufw allow in to any port 443
 
 # Enable UFW
 sudo ufw enable
