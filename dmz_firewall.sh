b4f25df0c8383aae2651d3e377be00ecb7fa5195
Giammo640 <g.lasalvia@studenti.unina.it>
2025-03-03 16:57:41 +0100

    Bug fixes

--- dmz_firewall.sh
+++ dmz_firewall.sh
@@ -1,29 +1,30 @@
 #!/bin/bash
 
 #Install UFW if it is not already installed
-sudo apt install -y ufw
+apt-get update
+apt install -y ufw
 
 echo "Fetching Facebook IP ranges..."
 IP_RANGES=$(whois AS32934 | grep -E "inetnum|NetRange" | awk '{print $2}')
 
 
 # Blocca tutto il traffico in entrata
-sudo ufw default deny incoming
+ufw default deny incoming
 
 
 # Block each IP range
 echo "Blocking Facebook IPs with UFW..."
 for IP in $IP_RANGES; do
-    sudo ufw deny out to $IP
-    sudo ufw deny in from $IP
+    ufw deny out to $IP
+    ufw deny in from $IP
     echo "Blocked: $IP"
 done
 
-sudo ufw allow in to any port 80
-sudo ufw allow in to any port 443
+ufw allow in to any port 80
+ufw allow in to any port 443
 
 # Enable UFW
-sudo ufw enable
+ufw enable
 
 # Reload UFW to apply changes
-sudo ufw reload
+ufw reload
